d6d232e94d081ec9da5ba9dfd9eef0027001de7d
FermionicChaos <z3r0caffein3@gmail.com>
2025-10-01 10:05:17 -0500

    modified some project props

---
+++ inc/geodesy/gpu.h
@@ -0,0 +1,7 @@
+#ifndef GEODESY_GPU_H
+#define GEODESY_GPU_H
+
+// Configuration for library
+#include "gpu/config.h"
+
+#endif // !GEODESY_GPU_H
--- inc/geodesy/gpu/config.h
+++ inc/geodesy/gpu/config.h
@@ -1,6 +1,25 @@
 #ifndef GEODESY_GPU_CONFIG_H
 #define GEODESY_GPU_CONFIG_H
 
+#include <vector>
+#include <set>
+#include <memory>
 
+#include <vulkan/vulkan.h>
+
+namespace geodesy {
+
+	// Template function to create shared_ptr with perfect forwarding
+	template<typename T, typename... Args>
+	std::shared_ptr<T> make(Args&&... args) {
+		return std::shared_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+
+	namespace gpu {
+		class instance;
+		class context;
+	}
+	
+}
 
 #endif // !GEODESY_GPU_CONFIG_H
