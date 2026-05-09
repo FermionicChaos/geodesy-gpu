10e1ea021d003ff05284064d5caa7635e6635efd
FermionicChaos <z3rocaffein3@fastmail.com>
2026-05-09 18:33:01 -0500

    added unit test

---
+++ tst/main.cpp
@@ -0,0 +1,11 @@
+// Unit test entry point for geodesy-gpu
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "geodesy-gpu unit tests starting..." << std::endl;
+
+    // Unit tests go here
+
+    return 0;
+}
